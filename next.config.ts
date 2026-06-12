b98c0bb6ebc24c2b03517eea2169319678af973d
BibekDhakal <bibekdkl1122@gmail.com>
2026-06-12 14:26:15 +0545

    Next.config Changed

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standalone", // <-- Change this line
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-
